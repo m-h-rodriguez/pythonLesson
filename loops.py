0c72acea7da65c911326f2adea051b26d0054639
m-h-rodriguez <105326343+m-h-rodriguez@users.noreply.github.com>
2022-05-31 19:31:39 -0400

    class demo

--- loops.py
+++ loops.py
@@ -1,35 +1,48 @@
-students = [
-    {
-        "fname": "Adrienne",
-        "lname": "Adams",
-    },
-    {
-        "fname": "Doug",
-        "lname": "Wiley",
-    },
-    {
-        "fname": "Emily",
-        "lname": "Coleman",
-    },
-    {
-        "fname": "Grace",
-        "lname": "Ensign",
-    },
-    {
-        "fname": "Katie",
-        "lname": "Cook",
-    },
-    {
-        "fname": "Kimberly",
-        "lname": "B",
-    },
-    {
-        "fname": "Mariah",
-        "lname": "Corso",
-    }
-]
+def load_students():
+    students = [
+        {
+            "fname": "Adrienne",
+            "lname": "Adams",
+        },
+        {
+            "fname": "Doug",
+            "lname": "Wiley",
+        },
+        {
+            "fname": "Emily",
+            "lname": "Coleman",
+        },
+        {
+            "fname": "Grace",
+            "lname": "Ensign",
+        },
+        {
+            "fname": "Katie",
+            "lname": "Cook",
+        },
+        {
+            "fname": "Kimberly",
+            "lname": "B",
+        },
+        {
+            "fname": "Mariah",
+            "lname": "Corso",
+        }
+    ]
+    return(students)
 
-for s in students:
-    print("Hello " + s["fname"] + " " + s["lname"])
-    input("What did you work on last week?")
-    print(f"Thanks so much {s['fname']}")
+
+def standup(students):
+    for student in students:
+        print("Hello " + student["fname"] + " " + student["lname"])
+        input("What did you work on last week?")
+        print(f"Thanks so much {student['fname']}")
+
+
+def main():
+    standup(students)
+    students = load_students()
+
+
+if __name__ == "__main__":
+    main()
